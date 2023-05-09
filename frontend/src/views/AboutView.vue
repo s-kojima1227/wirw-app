0df27d6f2adb4d5f82c7961fbef080e5071ec7b3
s_kojima <s_kojima@avelio.co.jp>
2023-05-09 09:50:59 +0900

    add: axiosによるapi通信基盤作成

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+
+const $http = inject('axios')
+const healthCheck = async (): Promise<void> => {
+  const response = await $http.get('http://localhost:3000/')
+  console.log(response.data)
+}
+healthCheck()
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
