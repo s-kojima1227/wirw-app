b0348abc22f32d904054c441db5b5375d2d85730
s_kojima <s_kojima@avelio.co.jp>
2023-05-13 00:07:54 +0900

    初期ファイル整理

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div
-    class="flex h-screen items-center justify-center bg-white dark:bg-gray-800"
-  >
-    <button
-      class="py-4 px-6 w-96 font-bold font-mono rounded-md transition-all text-white bg-blue-700 hover:bg-blue-800 active:bg-blue-900 dark:text-black dark:bg-blue-400 dark:hover:bg-blue-500 dark:active:bg-blue-600"
-    >
-      Hello Tailwind
-    </button>
-  </div>
+  <RouterView />
 </template>
 
 <style scoped>
---
+++ frontend/src/views/WishList.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+
+const $http = inject('axios')
+const healthCheck = async (): Promise<void> => {
+  const response = await $http.get('http://localhost:3000/')
+  console.log(response.data)
+}
+healthCheck()
+</script>
+
+<template>
+  <div class="wish-list bg-white py-6 sm:py-8 lg:py-12">
+    <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+      <!-- text - start -->
+      <div class="mb-10 md:mb-16">
+        <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">WISH LIST</h2>
+
+        <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">sample sample sample</p>
+      </div>
+      <!-- text - end -->
+
+      <div class="grid gap-4 sm:grid-cols-2 md:gap-8 xl:grid-cols-3">
+        <!-- feature - start -->
+        <div class="flex divide-x rounded-lg border bg-gray-50">
+          <div class="flex items-center p-2 text-indigo-500 md:p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+  
+          <div class="p-4 md:p-6">
+            <h3 class="mb-2 text-lg font-semibold md:text-xl">SAMPLE</h3>
+            <p class="text-gray-500">sample sample sample</p>
+          </div>
+        </div>
+        <!-- feature - end -->
+        <!-- feature - start -->
+        <div class="flex divide-x rounded-lg border bg-gray-50">
+          <div class="flex items-center p-2 text-indigo-500 md:p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+          <div class="p-4 md:p-6">
+            <h3 class="mb-2 text-lg font-semibold md:text-xl">SAMPLE</h3>
+            <p class="text-gray-500">sample sample sample</p>
+          </div>
+        </div>
+        <!-- feature - end -->
+        <!-- feature - start -->
+        <div class="flex divide-x rounded-lg border bg-gray-50">
+          <div class="flex items-center p-2 text-indigo-500 md:p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+          <div class="p-4 md:p-6">
+            <h3 class="mb-2 text-lg font-semibold md:text-xl">SAMPLE</h3>
+            <p class="text-gray-500">sample sample sample</p>
+          </div>
+        </div>
+        <!-- feature - end -->
+        <!-- feature - start -->
+        <div class="flex divide-x rounded-lg border bg-gray-50">
+          <div class="flex items-center p-2 text-indigo-500 md:p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+          <div class="p-4 md:p-6">
+            <h3 class="mb-2 text-lg font-semibold md:text-xl">SAMPLE</h3>
+            <p class="text-gray-500">sample sample sample</p>
+          </div>
+        </div>
+        <!-- feature - end -->
+        <!-- feature - start -->
+        <div class="flex divide-x rounded-lg border bg-gray-50">
+          <div class="flex items-center p-2 text-indigo-500 md:p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+          <div class="p-4 md:p-6">
+            <h3 class="mb-2 text-lg font-semibold md:text-xl">SAMPLE</h3>
+            <p class="text-gray-500">sample sample sample</p>
+          </div>
+        </div>
+        <!-- feature - end -->
+        <!-- feature - start -->
+        <div class="flex divide-x rounded-lg border bg-gray-50">
+          <div class="flex items-center p-2 text-indigo-500 md:p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 md:h-8 md:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
+          <div class="p-4 md:p-6">
+            <h3 class="mb-2 text-lg font-semibold md:text-xl">SAMPLE</h3>
+            <p class="text-gray-500">sample sample sample</p>
+          </div>
+        </div>
+        <!-- feature - end -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .wish-list {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
