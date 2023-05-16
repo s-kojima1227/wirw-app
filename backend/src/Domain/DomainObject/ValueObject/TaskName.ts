b12914a42bef606a5a9d9d3b07e2aea62dfd2f37
s_kojima <s_kojima@avelio.co.jp>
2023-05-16 22:48:39 +0900

    Add: DDDディレクトリ作成

---
+++ backend/src/Domain/DomainObject/ValueObject/TaskName.ts
@@ -0,0 +1,19 @@
+import { Exception } from '../../../Exception/exception';
+
+export class TaskName {
+  private _value: string;
+
+  get value(): string {
+    return this._value;
+  }
+
+  constructor(value: string) {
+    if (value.length === 0)
+      throw new Exception('Task name should not be empty.');
+
+    if (value.length > 20)
+      throw new Exception('Task name should not be longer than 20 characters.');
+
+    this._value = value;
+  }
+}
