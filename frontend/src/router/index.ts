ad40425420dfae93fdd5b6e6d173945dac1eafce
s_kojima <s_kojima@avelio.co.jp>
2023-05-20 23:44:24 +0900

    to p.128

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,9 +4,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/wish',
-      name: 'WishList',
-      component: () => import('../views/WishList.vue')
+      path: '/computed',
+      name: 'Computed',
+      component: () => import('../views/Computed.vue')
+    },
+    {
+      path: '/reactive',
+      name: 'Reactive',
+      component: () => import('../views/Reactive.vue')
+    },
+    {
+      path: '/bind-class',
+      name: 'BindClass',
+      component: () => import('../views/BindClass.vue')
     },
   ]
 })
---
+++ frontend/src/views/BindClass.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const msg = ref('Hello, World')
+const isTextRed = ref(true);
+const isBgColorBlue = ref(false);
+const styles = ref({
+  textColorRed: false,
+  bgColorBlue: true,
+});
+const computedStyles = computed((): {
+  textColorRed: boolean;
+  bgColorBlue: boolean;
+} => ({
+    textColorRed: Boolean(Math.round(Math.random())),
+    bgColorBlue: Boolean(Math.round(Math.random())),
+}));
+</script>
+
+<template>
+  <p :class="{ textColorRed: true, bgColorBlue: true }">
+    {{ msg }}
+  </p>
+  <p :class="{ textColorRed: isTextRed, bgColorBlue: isBgColorBlue }">
+    {{ msg }}
+  </p>
+  <p :class="{ textColorPink: true }">
+    {{ msg }}
+  </p>
+  <p :class="{ 'text-color-pink': true }">
+    {{ msg }}
+  </p>
+  <p class="textSize24" :class="{ textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue }">
+    {{ msg }}
+  </p>
+  <p class="textSize24" :class="styles">
+    {{ msg }}
+  </p>
+  <p :class="computedStyles">
+    {{ msg }}
+  </p>
+</template>
+
+<style>
+.textColorRed {
+  color: red;
+}
+.text-color-pink {
+  color: pink;
+}
+.bgColorBlue {
+  background-color: blue;
+}
+.textSize24 {
+  font-size: 24px;
+}
+</style>
